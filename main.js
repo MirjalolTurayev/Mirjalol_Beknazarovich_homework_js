b0ff1541ccd78c5362f1c55454b1c330de29ae8b
MirjalolTurayev <111414345+MirjalolTurayev@users.noreply.github.com>
2022-09-20 14:14:25 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,942 @@
+// 1 exercise
+// let a = 51;
+
+
+// if ( a > 0 && a % 1 == 0) {
+//     console.log("musbat butun son");    
+// } else if ( a < 0 && a % -1 == 0){
+//     console.log("manfiy butun son");
+// } else {
+//     console.log("kasr sonlar");
+// };
+
+
+// 2, 3 exercise
+
+// let a = 3;
+
+// if ( a > 0 && a % 2 ==0){
+//     console.log("musbat juft son");
+// } else if ( a < 0 && a % -2 == 0) {
+//     console.log("manfiy juft son");
+// } else if ( a > 0 && a % 2 ==1) {
+//     console.log("musbat toq son");
+// } else {
+//     console.log("manfiy toq son");
+// }
+
+
+
+// 4 exercise 
+
+// let a = 1,
+//     b = 2;
+
+
+// if ( a > 0 && a % 1 == 0 && b > 0 && b % 1 == 0){
+//     console.log("musbat butn sonlar");
+// } else if ( a < 0 && a % -1 == 0 && b < 0 && b % -1 == 0) {
+//     console.log("manfiy butun sonlar");
+// } else {
+//     console.log("butun sonlar emas");
+// }
+
+
+
+// 5 exercise 
+
+// let a = 3,
+//     b = 2,
+//     c = 1;
+
+// if ( a > b && b > c){
+//     console.log("shart bajarildi");
+// } else {
+//     console.log("false");
+// }
+
+
+
+// 6 exercise
+
+// let a = 1,
+//     b = 2,
+//     c = 3;
+
+
+// if ( a < b && b < c){
+//     console.log("shart bajarildi");
+// } else {
+//     console.log("false");
+// }
+
+// 7 exercise 
+
+// let a = 3,
+//     b = 5;
+
+// if ( a % 2 == 1 && b % 2 ==1){
+//     console.log("toq son");
+// } else {
+//     console.log("juft son");
+// };
+
+// 8 exercise
+
+// let a = 1,
+//     b = 2,
+//     c = 3,
+//     d = 4;
+
+// if ( a > 0 && b > 0 && c > 0 && d > 0){
+//     console.log("shart bajarildi");
+// } else {
+//     console.log(false);
+// };
+
+// 9 exercise 
+
+// let a = 1,
+//     b = 2,
+//     c = -3;
+
+// if ( a > 0 && b > 0 && c > 0){
+//     console.log("ok");
+// } else if (a > 0 && b > 0 && c <0){
+//     console.log("ok 1");
+// } else if ( a > 0 && b < 0 && c > 0){
+//     console.log("ok 2");
+// } else if ( a < 0 && b > 0 && c > 0) {
+//     console.log("ok 3");
+// } else {
+//     console.log(false);
+// }
+
+// 10 exercise
+
+// function weekDay( a ) {
+//     if ( a == 1){
+//         console.log("Monday");
+//     } else if ( a == 2 ){
+//         console.log("Tuesday")
+//     } else if ( a == 3){
+//         console.log("Wednesday");
+//     } else if ( a == 4){
+//         console.log("Thursday");
+//     } else if ( a == 5 ){
+//         console.log("Friday");
+//     } else if ( a == 6 ){
+//         console.log("Saturday");
+//     } else if ( a == 7){
+//         console.log("Sunday");
+//     } else {
+//         console.log("not days of the week");
+//     }    
+// }
+
+// weekDay( 7 )
+
+// let weekday = [ 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+// function week(a) {    
+//     console.log(weekday[a]);
+// }
+// week(0)
+
+// 11 exercise
+
+// function operator( kompaniya ) {
+//     if ( kompaniya.includes ("+99893")){
+//         console.log("Ucell");
+//     } else if ( kompaniya.includes ("+99890")){
+//         console.log("Beeline");
+//     } else if ( kompaniya.includes ("+99833")){
+//         console.log("Humans");
+//     } else if ( kompaniya.includes ("+99897")){
+//         console.log("Mobi Uz");
+//     } else if ( kompaniya.includes ("+99899")){
+//         console.log("Uztelecom");
+//     } else {
+//         console.log(false);
+//     }
+// };
+
+// operator( "+998337929494" )
+
+// 12 exercise
+
+// function myFunction(a){
+//     if ( a > 0 && a ++) {
+//         console.log([a])        
+//     } else if ( a < 0 && a ++ ){
+//         console.log([a]);
+//     }
+// }
+// myFunction(-15)
+
+
+// 13 exercise
+
+// function myFunction(a) {
+//     if ( a > 0 && a + 3  ){
+//         console.log([a +3]);
+//     } else if ( a < 0 && a + 2 ){
+//         console.log([a +2]);
+//     }
+// };
+// myFunction(-6) 
+
+// let a = -5,
+//     b = 3,
+//     d = 2;
+
+// if ( a > 0 && a + 3 ){
+//     console.log([a + 3]);
+// } else if ( a < 0 && a + 2){
+//     console.log([a + 2]);
+// };
+
+// 14 exercise
+
+// function myFunction( a, b){
+//     if ( a > b) {
+//         console.log("a");        
+//     } else if (a < b ) {
+//         console.log("b");
+//     }
+// }
+// myFunction(44, 40 )
+
+// 15 exercise
+
+// function myFunction( a, b, c){
+//     let r = "big number"
+//     if ( a > b && a > c ) {
+//         console.log(`${r}` +' '+ [a]);
+//     } else if ( b > a && b > c ) {
+//         console.log( `${r}` + " " + [b]);
+//     } else if ( c > a && c > b ) {
+//         console.log( `${r}` + ' ' + [c]);
+//     }
+// }
+// myFunction( 5, 1, 30 )
+
+// 16 exercise
+
+// function myFunction ( a, b, c){
+//     let r = "small number";
+//     if ( a < b && a < c) {
+//         console.log(`${r}` + ' ' + [a]);        
+//     } else if ( b < a && b < c){
+//         console.log(`${r}` + ' ' + [b]);
+//     } else if ( c < a && c < b) {
+//         console.log(`${r}` + ' ' + [c]);        
+//     }
+//     return
+// }
+
+// myFunction( 10, 20, 3)
+
+// 17 exercise 
+
+
+
+
+
+// 18 exercise
+
+// function stependiya( a ) {
+//     let r = "ming so'm"
+//     if ( a == 5 ){
+//         console.log('100' + ' ' + `${r}`);
+//     } else if ( a == 4 ){
+//         console.log('75' + ' ' + `${r}`);
+//     } else if ( a == 3){
+//         console.log('50' + ' ' + `${r}`);
+//     } else if ( a < 3 ){
+//         console.log('siz stependiya olishga layoqatsiz');
+//     }
+// }
+
+// stependiya(2)
+
+// 19 exercise
+
+// function myFunction(a) {
+//     if ( a > 0 && a % 2 == 0){
+//         console.log('musbat juft son')
+//     } else if ( a < 0 && a % 2 == 0) {
+//         console.log('manfiy juft son');        
+//     } else if ( a > 0 && a % 2 == 1){
+//         console.log('musbat toq son');
+//     } else if ( a < 0 && a % 2 == -1){
+//         console.log('manfiy toq son')
+//     }
+//     return    
+// };
+
+// myFunction(-3)
+
+// 20 exercise
+
+// function age(a) {
+//     let r = 'sizning yoshingiz'
+//     console.log(`${r}` + ' ' + [2022 - a]);
+// }
+// age(1996)
+
+// 21 exercise
+
+// function myFunction( a, b, c) {
+//     if ( a >= b >= c){
+//         console.log('ok');
+//     } else if ( a <= b <= c ){
+//         console.log('okey');
+//     } else {
+//         console.log('false');
+//     }
+//     return
+// }
+// myFunction( 10, 44, 3)
+
+// 22 exercise
+
+// function myFunction( a, b, c) {
+//     if ( a % 2 == 0 && b % 2 == 0 && c % 2 == 1) {
+//         console.log(true);
+//     } else if ( a % 2 == 0 && b % 2 == 1 && c % 2 == 0) {
+//         console.log('true 1');
+//     } else if ( a % 2 == 1 && b % 2 == 0 && c % 2 == 0){
+//         console.log('true 2');
+//     } else {
+//         console.log(false);
+//     }
+//     return
+// }
+// myFunction( 1, 2, 4)
+
+// 23 exercise
+
+// function myFunction ( a, b, c ){
+//     if ( a > 0 && b > 0 && c > 0) {
+//         console.log(true);        
+//     } else {
+//         console.log(false);
+//     }
+//     return
+// }
+// myFunction( 1, 2, 3 )
+
+// 24 exercise
+
+// function myFunction( a, b, c) {
+//     if ( a > 0 && b < 0 && c < 0) {
+//         console.log('ok');        
+//     } else if ( a < 0 && b > 0 && c < 0){
+//         console.log('okey');
+//     } else if ( a < 0 && b < 0 && c > 0){
+//         console.log('okey 1');
+//     } else {
+//         console.log(false);
+//     }
+//     return
+// };
+
+// myFunction( 10, -3, -2);
+
+// 25 exercise
+
+// function myFunction(a) {
+//     if ( a % 111 == 0 ) {
+//         console.log('false');        
+//     } else {
+//         console.log('true');
+//     }
+//     return
+// }
+
+// myFunction(202)
+
+// 26 exercise
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//FOR
+
+// 1 exercise
+
+// let k = 5;
+// let n = 15;   
+
+// for ( let i = 1; i <= n; i ++){
+//     console.log(k);
+// };
+
+// 2 exercise
+
+// let a = 50; 
+// let b = 40;
+
+// function myFunction() {
+//     for (let i = b; i < a + 1; i ++) {
+//         console.log(i);
+//     }    
+// }
+
+// myFunction()
+
+// 3 exercise
+
+// let a = 10;
+// let b = 2;
+
+// function myFunction() {
+//     for (let i = a-1; i >= b+1; i --){
+//         console.log(i);
+//     }
+//     return
+// }
+
+// myFunction()
+
+// 4 exercise
+
+// function myFuction( a, b) {
+
+//     for ( i = a; i <= b; i ++){
+//         console.log(i * 10000);
+//     }
+//     return
+// };
+
+// myFuction( 1, 10)
+
+// 5 exercise
+
+// function getnumber(a,b){
+//     for (i =a*10; i<=b*10; i ++){
+//         console.log(i*10000 /10 );
+//     }
+// };
+// getnumber (0.1,1);
+
+// 6 exercise
+
+// function myFunction(a,b){
+//     for ( i=a * 10 + 1; i <= b * 10; i++){
+//         if ( i % 2 == 0){
+//             console.log( i *10000 / 10);
+//         }
+//     }
+// };
+
+// myFunction (1,2);
+
+// 7 exercise
+
+// function myFunction( a, b){
+//     let result = a - a;
+//     for( i = a; i <= b; i ++ ){
+//         result = result + i;
+//     }
+//     return console.log(result);
+// };
+
+// myFunction(10, 15);
+
+// 8 exercise 
+
+// function myFunction( a, b){
+//     let result = a;
+//     for ( i = a; i <= b; i ++ ){
+//         result = result *i
+//     }
+//     return console.log(result);
+// };
+
+// myFunction( 5, 10);
+
+// 9 exercise
+
+// function myFunction( a, b ){
+//     let result = 0;
+//     for ( i = a; i <= b; i ++){
+//         console.log(i);
+//        result = result + (i * i);
+//     }
+//     return console.log(result);
+// };
+// myFunction( 2, 6);
+
+// console.log(result);
+
+// 10 exercise
+
+// function myFunction ( n ){
+//     let result = 0;
+//     for ( i = 1; i <= n; i ++){
+//         result = result + ( 1 / i );
+//     }
+//     return console.log(result);
+// }
+
+// myFunction( 10 );
+
+// 11 exercise
+
+// function myFunction (n){
+//     for ( i = 1; i <= n; i ++){
+//         if ( i % 2 == 1){
+//             console.log(i);
+//         }
+//     }
+// };
+
+// myFunction(10)
+
+// 12 exercise 
+
+// function myFunction (n) {
+//     for ( i = 1; i <= n; i ++ ){
+//         if ( i % 2 == 0){
+//             console.log(i);
+//         }
+//     }
+// }
+
+// myFunction(10)
+
+// 13 exercise
+
+// function myFunction (n){
+//     for ( i = 1; i <= n; i ++){
+//         if ( i % 1 == 0 && i % 2 == 0 && i % 4 == 0){
+//             console.log(i);
+//         }
+//     }
+// };
+
+// myFunction(10)
+
+// 14 exercise
+
+// function myFunction (n){
+//     for ( i = 1; i <= n; i ++ ){
+//         if ( i % 2 == 0 ){
+//             console.log(i * i);
+//         } else if ( i % 2 ==1){
+//             console.log(i * i);
+//         }
+//     }
+//     return
+// };
+
+// myFunction (10);
+
+// 15 exercise 
+
+// function myFunction (n){
+//     for ( i = n; i >= 1; i --){
+//         console.log(i);
+//     }
+//     return
+// };
+
+// myFunction(10)
+
+// 16 exercise
+
+// function myFunction (n){
+//     for ( i = 1; i <= n; i ++){
+//         // console.log(i);
+//         if ( i % 5 !== 0 ){
+//             console.log(i);
+//         } 
+//     } 
+// };
+
+// myFunction (30)
+
+// 17 exercise
+
+// function myFunction(n){
+//     let result = 1; 
+//     for ( i = 1; i <= n; i ++){
+//         result = result * i;
+//     }
+//     return console.log(result);
+// };
+
+// myFunction(10)
+
+// exercise 18
+
+// exercise 19
+
+// function myFuction ( n ){
+//     let result = 0;
+//     let x = 2;
+//     for ( i = 1; i <= n; i++  ){
+//         result = result + !x * i / i;       
+//     };
+//     return console.log(result);
+// };
+
+// myFuction ( 10)
+
+// exercise 20
+
+// function myFunction( n, x ) {
+//     for ( i = 1; i <= n; i++ ){
+//         if ( i % x ==0) {
+//             console.log(i);            
+//         }
+//     }
+//     return    
+// };
+
+// myFunction (100, 15)
+
+// exercise 21
+
+// function myFunction ( n, x){
+//     let result = 0;
+//     let resultN = 0;
+
+//     for ( i = 1; i <= x; i++ ){
+//         result = result + i;
+//         for (a = 1; a <= n; a++ ){
+//             resultN =resultN + a * result;
+//         };
+//     };
+//     return console.log(result, resultN);
+// };
+
+// myFunction( 3, 5)
+
+
+
+
+
+
+
+
+
+
+
+
+// Switch Case
+
+// 1 exercise
+
+// let hafta = '1';
+
+// switch (hafta){
+//     case '1':
+//         console.log('Monday');
+//         break
+//     case '2':
+//         console.log('Tuesday');
+//         break
+//     case '3':
+//         console.log('Wednesday');
+//         break
+//     case '4':
+//         console.log('Thursday');
+//         break
+//     case '5':
+//         console.log('Friday');
+//         break
+//     case '6':
+//         console.log('Saturday');
+//         break
+//     case '7':
+//         console.log('Sunday');
+//         break
+//     default:
+//     console.log('xafta kunini kirgizishda adashdingiz');
+// };
+
+// 2 exercise
+
+// let k = '5';
+
+// switch (k){
+//     case '1':
+//         console.log('yomon');
+//         break
+//     case '2':
+//         console.log('qoniqarsiz');
+//         break
+//     case '3':
+//         console.log('qoniqarli');
+//         break
+//     case '4':
+//         console.log('yaxshi');
+//         break
+//     case '5':
+//         console.log('alo');
+//         break
+//     default:
+//         console.log('baxoni kiritishda adashtingiz');
+// };
+
+// 3 exercise
+// Oy raqami berilgan Kiritilgan oy qaysi faslga tegishli ekanin chiqaruvchi dastur tuzing. M - s 2 - qish
+
+// let fasl = '5';
+
+// switch ( fasl ){
+//     case '12':
+//         console.log('winter');
+//         break
+//     case '1':
+//         console.log('winter');
+//         break
+//     case '2':
+//         console.log('winter');
+//         break
+//     case '3':
+//         console.log('spring');
+//         break
+//     case '4':
+//         console.log('spring');
+//         break
+//     case '5':
+//         console.log('spring');
+//         break
+//     case '6':
+//         console.log('summer');
+//         break
+//     case '7':
+//         console.log('summer');
+//         break
+//     case '8':
+//         console.log('summer');
+//         break
+//     case '9':
+//         console.log('autumn');
+//         break
+//     case '10':
+//         console.log('autumn');
+//         break
+//     case '11':
+//         console.log('autumn');
+//         break
+//     default:
+//         console.log('siz fasl raqamini notugri kirgizdingiz');
+// };
+
+// 4 exercise
+
+// let oy = '5';
+
+// switch ( oy ){
+//     case '12':
+//         console.log('31');
+//         break
+//     case '1':
+//         console.log('31');
+//         break
+//     case '2':
+//         console.log('28');
+//         break
+//     case '3':
+//         console.log('31');
+//         break
+//     case '4':
+//         console.log('30');
+//         break
+//     case '5':
+//         console.log('31');
+//         break
+//     case '6':
+//         console.log('30');
+//         break
+//     case '7':
+//         console.log('31');
+//         break
+//     case '8':
+//         console.log('31');
+//         break
+//     case '9':
+//         console.log('30');
+//         break
+//     case '10':
+//         console.log('31');
+//         break
+//     case '11':
+//         console.log('30');
+//         break
+//     default:
+//         console.log('siz oy raqamini notugri kirgizdingiz');
+// };
+
+// 5 exercise
+
+// let a = 10;
+// let b = 5;
+
+// 6 exercise
+
+// let k = '5';
+
+// switch (k){
+//     case '1':
+//         console.log('sizning stipendiya olishga layoqatli emassiz');
+//         break
+//     case '2':
+//         console.log('sizning stipendiya olishga layoqatli emassiz');
+//         break
+//     case '3':
+//         console.log('60 000 ming');
+//         break
+//     case '4':
+//         console.log('80 000 ming');
+//         break
+//     case '5':
+//         console.log('100 000 ming');
+//         break
+//     default:
+//         console.log('baxoni kiritishda adashtingiz');
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let array = [1, 10, null, -20, 25, undefined, 'salom', 9, '5', false]  // --> 45
+
+// let result = 0;
+// for ( let i = 0; i < array.length; i++){
+//     // console.log(array[i]);
+//     if (  array[i] > 0 && typeof (array[i]) === "number"){
+                
+//         result = result + array[i]
+//     }
+// }
+
+// console.log(result);
+
+
+
+// let BigArr = ["Iphone 11 Pro ios", "A 51 sam", "Iphone 12 Pro ios", "S 20 ultra sam", "Iphone 14 Pro ios","A 12 mini sam"];
+
+// function search ( array){
+
+//     let samsun = [];
+//     let IOS = [];
+
+//     for ( let i = 0; i < array.length; i++){
+//         // console.log( array[i]);
+//         if ( array [i].includes ("sam")){
+//             samsun.push (array[i]);
+//         } else if ( array[i].includes ("ios")){
+//             IOS.push (array[i]);
+//         }
+//     }
+//     return [samsun, IOS]
+// };
+// console.log(search ( BigArr));
+
